28a150d5dbf8bb45d057783722b1ed2028aa3c43
Rafael Aguilar Muñoz <rafaaguilarmunoz@gmail.com>
2022-07-29 11:47:44 +0200

    Add files via upload

--- todolist/app/Http/Controllers/EtiquetaController.php
+++ todolist/app/Http/Controllers/EtiquetaController.php
@@ -7,12 +7,84 @@ use Illuminate\Http\Request;
 class EtiquetaController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $etiqueta = \App\Models\Etiqueta::all();
+
+      return view('etiqueta', ['allEtiqueta' => $etiqueta]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      return view('etiqueta');
+    }
+
+    /**
+     * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function store(Request $request) {
+
+      \App\Models\Etiqueta::create([
+        'etiqueta' => $request->get('etiqueta'),
+        'color' => $request->get('color'),
+      ]);
+
+      return redirect('/etiqueta');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      return view("etiqueta", ["etiqueta" => $id]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
     {
         //
     }
